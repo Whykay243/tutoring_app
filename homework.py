79ff89133a2a832b39f9c62bf2bc77eb6c84956b
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-18 00:01:00 -0400

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,22 +1,23 @@
-from flask import request, render_template, redirect, url_for
-from flask_login import login_required, current_user
+from flask import Blueprint, request, render_template, redirect, url_for, current_app
+from flask_login import login_required
 from werkzeug.utils import secure_filename
-from app import app
 import os
 
-@app.route('/dashboard')
+homework = Blueprint('homework', __name__)
+
+@homework.route('/dashboard')
 @login_required
 def dashboard():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    files = os.listdir(current_app.config['UPLOAD_FOLDER'])
     return render_template('dashboard.html', files=files)
 
-@app.route('/upload', methods=['GET', 'POST'])
+@homework.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
     if request.method == 'POST':
-        file = request.files['pdf']
-        if file.filename.endswith('.pdf'):
+        file = request.files.get('pdf')
+        if file and file.filename.endswith('.pdf'):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('dashboard'))
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('homework.dashboard'))
     return render_template('upload.html')
